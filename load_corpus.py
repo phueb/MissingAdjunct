d410f4bad2326092d24d0c2025f613f5b9d53668
phueb <info@philhuebner.com>
2021-10-29 09:29:41 -0500

    add gen_sentences()

--- load_corpus.py
+++ load_corpus.py
@@ -3,4 +3,9 @@ from missingadjunct.corpus import Corpus
 
 corpus = Corpus.load()
 
-print(corpus.forms)
+corpus.print_info()
+
+for sentence in corpus.gen_sentences():
+
+    words = [f'{w:<12}' for w in sentence.split()]
+    print(' '.join(words))
--- missingadjunct/corpus.py
+++ missingadjunct/corpus.py
@@ -1,25 +1,31 @@
 import pickle
-from typing import Optional
+from typing import Optional, Generator, List, Tuple
 import datetime
 
 from missingadjunct import configs
+from items import LogicalForm
 
 
 FILE_NAME = 'missingadjunct_corpus'
+WS = ' '
+
+
+def get_date():
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d")
 
 
 class Corpus:
     def __init__(self):
-        self.forms = []
+        self.forms: List[LogicalForm] = []
 
         self.params = {}  # TODO
 
+        self.date = get_date()
+
     def save(self):
 
-        now = datetime.datetime.now()
-        date_string = now.strftime("%Y-%m-%d")
-        fn = f'{FILE_NAME}_{date_string}.pkl'
-
+        fn = f'{FILE_NAME}_{self.date}.pkl'
         path_out = (configs.Dirs.pickles / fn)
         with path_out.open('wb') as file:
             pickle.dump(self, file)
@@ -28,13 +34,12 @@ class Corpus:
 
     @classmethod
     def load(cls,
-             date_string: Optional[str] = None,
+             date: Optional[str] = None,
              ):
 
-        if date_string is None:
-            now = datetime.datetime.now()
-            date_string = now.strftime("%Y-%m-%d")
-        fn = f'{FILE_NAME}_{date_string}.pkl'
+        if date is None:
+            date = get_date()
+        fn = f'{FILE_NAME}_{date}.pkl'
 
         path_out = (configs.Dirs.pickles / fn)
         with path_out.open('rb') as file:
@@ -44,3 +49,31 @@ class Corpus:
 
     def plot_stats(self):
         pass
+
+    def print_info(self):
+        print('Info about corpus:')
+        print(f'date={self.date}')
+
+    def gen_sentences(self,
+                      num_epochs: int = 1,
+                      ) -> Generator[str, None, None]:
+        for epoch in range(num_epochs):
+            for lf in self.forms:
+
+                sentence = lf.agent + WS + lf.verb + WS + lf.theme + WS
+
+                if lf.instrument:
+                    sentence += 'with' + WS + lf.instrument + WS
+
+                if lf.location:
+                    sentence += 'in' + WS + lf.location + WS
+
+                yield sentence
+
+    def gen_trees(self,
+                  num_epochs: int = 1,
+                  ) -> Generator[Tuple, None, None]:
+        for epoch in range(num_epochs):
+            for lf in self.forms:
+                tree = ()
+                yield tree
